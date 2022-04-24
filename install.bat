32ad693aa0998a50f615d47356a33261b23c3bd9
mattscarce <104291584+mattscarce@users.noreply.github.com>
2022-04-24 01:45:02 -0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo off
+pip install platform
+pip install requests
+pip install sys
+pip install os
+pip install wheel
+pip install fade
+pip install gratient
+pip install urllib3
+pip install threading
+pip install pystyle
+pause
